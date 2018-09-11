ef68994c8bf37dbd3c17285340d6be9dd6c8f8d1
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2018-09-10 22:08:10 -0400

    Get a basline version of the command working and move from eslint to xo.

--- features/step_definitions/fake-data-generator.js
+++ features/step_definitions/fake-data-generator.js
@@ -1,18 +1,18 @@
-const { Given, When, Then } = require("cucumber");
-const { expect } = require("chai");
 const { existsSync } = require("fs");
 const path = require("path");
+const { Given, When, Then } = require("cucumber");
+const { expect } = require("chai");
 
-Given("a file called {string}", function (string) {
+Given("a file called {string}", string => {
   expect(existsSync(path.join(string))).to.eql(true);
 });
 
-When("it has a column specification", function () {
+When("it has a column specification", () => {
   // Write code here that turns the phrase above into concrete actions
   return "pending";
 });
 
-Then("it generates some data", function () {
+Then("it generates some data", () => {
   // Write code here that turns the phrase above into concrete actions
   return "pending";
 });
